694e2e54c7c0cbf96ba2e6b6fdfd78ad00ed6265
Francis Elendu <elcfran6@hotmail.com>
2026-02-16 23:36:04 -0600

    added form

--- src/app/features/category/add-category/add-category.ts
+++ src/app/features/category/add-category/add-category.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-category.html',
   styleUrl: './add-category.css',
 })
 export class AddCategory {
+// 1. import ReactiveFormsModule
+// 2. create form group and form control (FormGroups -> FormControls)
+// 3. bind form group to form element in html
+// 4. bind form controls to input elements in html
+// 5. submit form and get form values
+
+addCategoryFormGroup = new FormGroup({
+  name: new FormControl<string>('', { nonNullable: true }),
+  urlHandle: new FormControl<string>('', { nonNullable: true }),
+});
+
+onSubmit() {
+  console.log(this.addCategoryFormGroup.value);
+  console.log(this.addCategoryFormGroup.getRawValue());
+
+}
+
 
 }
